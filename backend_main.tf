d44bf0aaea1a03a2cbbe2e912bd3b2c12aab5f21
Balaji Bandlapalli <balaji.bandlapalli@entropiktech.com>
2023-01-24 17:00:05 +0530

    AB#24558: create CloudFront distribution for the media rendering

--- backend_main.tf
+++ backend_main.tf
@@ -20,7 +20,7 @@ module "create_eu_s3_bucket" {
   datacenter_codes       = var.datacenter_codes
   tester_view_sub_domain = var.tester_view_sub_domain
   base_domain            = var.base_domain
-
+  
 
   providers = {
     aws.s3_region = aws.eu_region
@@ -38,6 +38,24 @@ module "create_eu_acm_media_cf" {
   providers = {
     aws.acm_region        = aws.us_region
     aws.datacenter_region = aws.eu_region
+  }
+}
+
+module "create_eu_media_cloudfront" {
+  source                      = "./cloudfront"
+  base_domain                 = var.base_domain
+  sub_domain                  = var.media_sub_domain
+  datacenter_codes            = var.datacenter_codes
+  bucket_arn                  = module.create_eu_s3_bucket.s3_bucket_arn
+  bucket_id                   = module.create_eu_s3_bucket.s3_bucket_id
+  bucket_regional_domain_name = module.create_eu_s3_bucket.s3_bucket_regional_domain_name
+  acm_certificate_arn         = module.create_eu_acm_media_cf.acm_arn
+  DEFAULT_TAGS                = var.DEFAULT_TAGS
+  STAGE                       = var.STAGE
+
+  providers = {
+    aws.cloudfront_region = aws.us_region
+    aws.bucket_region     = aws.eu_region
   }
 }
 
@@ -77,7 +95,7 @@ module "create_in_s3_bucket" {
   datacenter_codes       = var.datacenter_codes
   tester_view_sub_domain = var.tester_view_sub_domain
   base_domain            = var.base_domain
-
+  
 
   providers = {
     aws.s3_region = aws.in_region
@@ -95,6 +113,24 @@ module "create_in_acm_media_cf" {
   providers = {
     aws.acm_region        = aws.us_region
     aws.datacenter_region = aws.in_region
+  }
+}
+
+module "create_in_media_cloudfront" {
+  source                      = "./cloudfront"
+  base_domain                 = var.base_domain
+  sub_domain                  = var.media_sub_domain
+  datacenter_codes            = var.datacenter_codes
+  bucket_arn                  = module.create_in_s3_bucket.s3_bucket_arn
+  bucket_id                   = module.create_in_s3_bucket.s3_bucket_id
+  bucket_regional_domain_name = module.create_in_s3_bucket.s3_bucket_regional_domain_name
+  acm_certificate_arn         = module.create_in_acm_media_cf.acm_arn
+  DEFAULT_TAGS                = var.DEFAULT_TAGS
+  STAGE                       = var.STAGE
+
+  providers = {
+    aws.cloudfront_region = aws.us_region
+    aws.bucket_region     = aws.in_region
   }
 }
 
@@ -134,7 +170,7 @@ module "create_sea_s3_bucket" {
   datacenter_codes       = var.datacenter_codes
   tester_view_sub_domain = var.tester_view_sub_domain
   base_domain            = var.base_domain
-
+  
 
   providers = {
     aws.s3_region = aws.sea_region
@@ -152,6 +188,24 @@ module "create_sea_acm_media_cf" {
   providers = {
     aws.acm_region        = aws.us_region
     aws.datacenter_region = aws.sea_region
+  }
+}
+
+module "create_sea_media_cloudfront" {
+  source                      = "./cloudfront"
+  base_domain                 = var.base_domain
+  sub_domain                  = var.media_sub_domain
+  datacenter_codes            = var.datacenter_codes
+  bucket_arn                  = module.create_sea_s3_bucket.s3_bucket_arn
+  bucket_id                   = module.create_sea_s3_bucket.s3_bucket_id
+  bucket_regional_domain_name = module.create_sea_s3_bucket.s3_bucket_regional_domain_name
+  acm_certificate_arn         = module.create_sea_acm_media_cf.acm_arn
+  DEFAULT_TAGS                = var.DEFAULT_TAGS
+  STAGE                       = var.STAGE
+
+  providers = {
+    aws.cloudfront_region = aws.us_region
+    aws.bucket_region     = aws.sea_region
   }
 }
 
@@ -191,7 +245,7 @@ module "create_us_s3_bucket" {
   datacenter_codes       = var.datacenter_codes
   tester_view_sub_domain = var.tester_view_sub_domain
   base_domain            = var.base_domain
-
+  
 
   providers = {
     aws.s3_region = aws.us_region
@@ -209,6 +263,24 @@ module "create_us_acm_media_cf" {
   providers = {
     aws.acm_region        = aws.us_region
     aws.datacenter_region = aws.us_region
+  }
+}
+
+module "create_us_media_cloudfront" {
+  source                      = "./cloudfront"
+  base_domain                 = var.base_domain
+  sub_domain                  = var.media_sub_domain
+  datacenter_codes            = var.datacenter_codes
+  bucket_arn                  = module.create_us_s3_bucket.s3_bucket_arn
+  bucket_id                   = module.create_us_s3_bucket.s3_bucket_id
+  bucket_regional_domain_name = module.create_us_s3_bucket.s3_bucket_regional_domain_name
+  acm_certificate_arn         = module.create_us_acm_media_cf.acm_arn
+  DEFAULT_TAGS                = var.DEFAULT_TAGS
+  STAGE                       = var.STAGE
+
+  providers = {
+    aws.cloudfront_region = aws.us_region
+    aws.bucket_region     = aws.us_region
   }
 }
 
---
+++ cloudfront/variables.tf
@@ -0,0 +1,45 @@
+variable "bucket_arn" {
+  type        = string
+  description = "s3 bucket arn"
+}
+
+variable "bucket_regional_domain_name" {
+  type        = string
+  description = "s3 bucket regional domain name"
+}
+
+variable "bucket_id" {
+  type        = string
+  description = "s3 bucket id"
+}
+
+variable "datacenter_codes" {
+  type        = map(string)
+  description = "Data center code values"
+}
+
+variable "sub_domain" {
+  type        = string
+  description = "sub domain name"
+}
+
+variable "base_domain" {
+  type        = string
+  description = "base domain name"
+}
+
+variable "acm_certificate_arn" {
+  type        = string
+  description = "ACM ARN for CloudFront"
+
+}
+variable "STAGE" {
+  type        = string
+  description = "Stage for deployment"
+}
+
+variable "DEFAULT_TAGS" {
+  type        = map(any)
+  description = "Default Tags for all resources"
+}
+
