cb05ee582cff520d8fb546d0c104695b09890453
Balaji Bandlapalli <balaji.bandlapalli@entropiktech.com>
2023-03-27 15:28:03 +0530

    AB#27679: add terraform provider version to be <=4.59.0

--- acm/main.tf
+++ acm/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
+      version               = "<= 4.59.0"
       configuration_aliases = [aws.acm_region, aws.datacenter_region]
     }
   }
