07972df7c1a1192f261899e6aa4c9095698452a2
sravs2510 <sravanipandu2510>
2023-03-29 17:53:30 +0530

    activate rule set

--- backend_main.tf
+++ backend_main.tf
@@ -651,19 +651,6 @@ module "create_iam" {
 }
 
 module "create_ses" {
-  count        = var.STAGE == "prod" ? 0 : 1
-  source       = "./ses"
-  STAGE        = var.STAGE
-  DEFAULT_TAGS = var.DEFAULT_TAGS
-  base_domain  = var.base_domain
-
-  providers = {
-    aws.ses_region = aws.us_region
-  }
-}
-
-module "create_ses_prod" {
-  count        = var.STAGE == "prod" ? 1 : 0
   source       = "./ses"
   STAGE        = var.STAGE
   DEFAULT_TAGS = var.DEFAULT_TAGS
