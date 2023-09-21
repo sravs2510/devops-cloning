3af4febaf00b10cfa5c71ba7c31e7676c01bdd4d
sravanibestha <sravanibestha@entropik.io>
2023-09-21 13:09:31 +0530

    renamed the tag & image name for cyborg

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -12,13 +12,13 @@ data "aws_region" "ecr_region" {
 }
 
 locals {
-  ecr_repo_name = join("-", [var.repo_name, "ecr"])
+  ecr_repo_tag_name = join("-", [var.repo_name, "ecr"])
 }
 resource "aws_ecr_repository" "qatalyst_repository" {
   # as Jenkins is in SEA Region
   provider = aws.ecr_region
   name     = var.repo_name
-  tags     = merge(tomap({ "Name" : local.ecr_repo_name }), tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)
+  tags     = merge(tomap({ "Name" : local.ecr_repo_tag_name }), tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)
 }
 
 resource "aws_ecr_lifecycle_policy" "qatalyst_repository_lifecycle" {
