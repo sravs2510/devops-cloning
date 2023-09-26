74854d4baa430108f2510cadb15b9d1a002b4313
sravanibestha <sravanibestha@entropik.io>
2023-09-26 14:20:11 +0530

    changed the stage to playground

--- terraform_backend.tf
+++ terraform_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    dynamodb_table = "entropik-terraform-state-lock-prestage"
-    bucket         = "entropik-terraform-state-prestage"
+    dynamodb_table = "entropik-terraform-state-lock-dev"
+    bucket         = "entropik-terraform-state-dev"
     key            = "terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = false
