2a9d7e99020081505459af981ae69c502d4cbba2
Balaji Bandlapalli <balaji.bandlapalli@entropiktech.com>
2024-02-22 14:55:01 +0400

    AB#40407 keep last 5 images in ecr

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -13,45 +13,6 @@ data "aws_region" "ecr_region" {
 
 locals {
   repo_name = join("-", ["qatalyst", var.service_name])
-  # DEV Policy
-  dev_ecr_lcp = jsonencode(
-    {
-      rules = [
-        {
-          rulePriority = 1
-          description  = "Keep last 1 untagged image"
-          selection = {
-            tagStatus   = "untagged",
-            countType   = "imageCountMoreThan",
-            countNumber = 1
-          }
-          action = {
-            type = "expire"
-          }
-        }
-      ]
-    }
-  )
-  #Non Dev Policy
-  ecr_lcp = jsonencode(
-    {
-      rules = [
-        {
-          rulePriority = 1
-          description  = "Keep last 5 tagged images"
-          selection = {
-            tagStatus     = "tagged"
-            tagPrefixList = ["20", "latest"]
-            countType     = "imageCountMoreThan"
-            countNumber   = 5
-          }
-          action = {
-            type = "expire"
-          }
-        }
-      ]
-    }
-  )
 }
 
 resource "aws_ecr_repository" "qatalyst_repository" {
@@ -64,5 +25,22 @@ resource "aws_ecr_repository" "qatalyst_repository" {
 resource "aws_ecr_lifecycle_policy" "qatalyst_repository_lifecycle" {
   provider   = aws.ecr_region
   repository = aws_ecr_repository.qatalyst_repository.name
-  policy     = var.STAGE == "dev" ? local.dev_ecr_lcp : local.ecr_lcp
+  policy = jsonencode(
+    {
+      rules = [
+        {
+          rulePriority = 1
+          description  = "Keep last 5  image"
+          selection = {
+            tagStatus   = "any",
+            countType   = "imageCountMoreThan",
+            countNumber = 5
+          }
+          action = {
+            type = "expire"
+          }
+        }
+      ]
+    }
+  )
 }
