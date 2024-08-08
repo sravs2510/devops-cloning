ba7f42c8278cac2b3da6a478b7869ae38846ee7d
shanmugapriyan.entropik <shanmuga.priyan@entropik.io>
2024-08-08 17:10:31 +0530

    AB#45291: [BE] VAPT issue tracker - TLS version change V1 to V2

--- modules/cloudfront-fe-wc/main.tf
+++ modules/cloudfront-fe-wc/main.tf
@@ -79,6 +79,7 @@ resource "aws_cloudfront_distribution" "media_cf_distribution" {
     cloudfront_default_certificate = false
     acm_certificate_arn            = var.acm_certificate_arn
     ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
 
   tags = merge(tomap({ "Name" : var.base_domain }), tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)
