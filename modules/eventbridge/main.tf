c974ef5dcb6912c6bd1f8f68fb26e7702a49cf9e
shanmugapriyan.entropik <shanmuga.priyan@entropik.io>
2024-09-03 11:11:41 +0530

    AB#42087: [Co-pilot] Pre-compute dataframe for co-pilot

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -8,21 +8,15 @@ terraform {
 }
 
 locals {
-  group_name = join("-", ["qatalyst", var.service])
+  schedule_groups = {
+    group_name = join("-", ["qatalyst", var.service])
+    schedule_group_name = join("-", ["qatalyst", var.schedule_group])
+  }
 }
 
 resource "aws_scheduler_schedule_group" "qatalyst_schedule_group" {
+  for_each = local.schedule_groups
   provider = aws.eventbridge_region
-  name     = local.group_name
-  tags     = merge(tomap({ "Name" : local.group_name }), tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)
-}
-
-locals {
-  schedule_group_name = join("-", ["qatalyst", var.schedule_group])
-}
-
-resource "aws_scheduler_schedule_group" "qatalyst_schedule_group" {
-  provider = aws.eventbridge_region
-  name     = local.schedule_group_name
+  name     = local.value
   tags     = merge(tomap({ "Name" : local.group_name }), tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)
 }
