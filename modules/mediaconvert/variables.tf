86949eb1c8ec9623e5b07268279d14d8497cd70a
SubrotoDas <subroto.das@entropik.io>
2024-11-04 12:44:11 +0530

    Migrated mediaconver module

--- modules/mediaconvert/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "mediaconvert_queues" {
-  type        = map(any)
-  description = "Mediaconvert Queues"
-}
-
-variable "DEFAULT_TAGS" {
-  type        = map(any)
-  description = "Default Tags for all resources"
-}
