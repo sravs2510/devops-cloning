17bad58d5c51edfbe1e989a76ce37e773993fa6e
Sravani <sravani@Sravanis-MacBook-Air.local>
2024-12-11 13:37:50 +0530

    secure values of ssm moved to local file

--- backend_main.tf
+++ backend_main.tf
@@ -1,3 +1,29 @@
+
+locals {
+  qatalyst_ssm_secure_values = {
+    join("-", ["qatalyst", var.STAGE, "bitly-bearer-token"])    = "#BITLY_BEARER_TOKEN"
+    join("-", ["qatalyst", var.STAGE, "sendgrid-key"])          = "#SENDGRID_KEY"
+    join("-", ["qatalyst", var.STAGE, "figma-access-token"])    = "#FIGMA_ACCESS_TOKEN"
+    join("-", ["qatalyst", var.STAGE, "fingerprint-token"])     = "#FINGERPRINT_API_TOKEN"
+    join("-", ["qatalyst", var.STAGE, "100ms-access-key"])      = "#QATALYST_100MS_ACCESS_KEY"
+    join("-", ["qatalyst", var.STAGE, "100ms-secret-key"])      = "#QATALYST_100MS_SECRET_KEY"
+    join("-", ["platform", var.STAGE, "client-id-in"])          = "#PLATFORM_CLIENT_ID_IN"
+    join("-", ["platform", var.STAGE, "client-id-us"])          = "#PLATFORM_CLIENT_ID_US"
+    join("-", ["platform", var.STAGE, "client-id-eu"])          = "#PLATFORM_CLIENT_ID_EU"
+    join("-", ["platform", var.STAGE, "client-id-sea"])         = "#PLATFORM_CLIENT_ID_SEA"
+    join("-", ["platform", var.STAGE, "secret-in"])             = "#PLATFORM_SECRET_IN"
+    join("-", ["platform", var.STAGE, "secret-us"])             = "#PLATFORM_SECRET_US"
+    join("-", ["platform", var.STAGE, "secret-eu"])             = "#PLATFORM_SECRET_EU"
+    join("-", ["platform", var.STAGE, "secret-sea"])            = "#PLATFORM_SECRET_SEA"
+    join("-", ["platform", var.STAGE, "realm-id"])              = "#PLATFORM_REALM_ID"
+    join("-", ["qatalyst", var.STAGE, "stripe-api-key"])        = "#QATALYST_STRIPE_API_KEY"
+    join("-", ["qatalyst", var.STAGE, "stripe-webhook-secret"]) = "#QATALYST_STRIPE_WEBHOOK_SECRET"
+    join("-", ["qatalyst", "lucid", "sha1", "key"])             = "#LUCID_SHA1_KEY"
+    join("-", ["qatalyst", var.STAGE, "g2-api-token"])          = "#QATALYST_G2_API_TOKEN"
+    join("-", ["qatalyst", var.STAGE, "g2-product-id"])         = "#QATALYST_G2_PRODUCT_ID"
+    join("-", ["qatalyst", var.STAGE, "google-credentials"])    = "#GOOGLE_AUTHENTICATION_DATA"
+  }
+}
 # EU Resources
 
 module "create_eu_vpc" {
@@ -161,8 +187,9 @@ module "create_eu_ssm" {
     },
     secure_parameters = merge(local.qatalyst_ssm_secure_values,
       {
-        join("-", ["qatalyst", var.STAGE, "open-ai-key"]) = lookup(var.open_ai_api, data.aws_region.eu.name)
-        "qatalyst-dashboard-opensearch-endpoint"          = join("", ["https://", try(module.create_eu_opensearch[0].opensearch_host, "")])
+        join("-", ["qatalyst", var.STAGE, "open-ai-key"])       = lookup(var.open_ai_api, data.aws_region.eu.name)
+        "qatalyst-dashboard-opensearch-endpoint"                = join("", ["https://", try(module.create_eu_opensearch[0].opensearch_host, "")])
+        join("-", ["qatalyst", var.STAGE, "feature-flag-auth"]) = random_uuid.feature_flag_auth_eu.result
     })
   }
   providers = {
@@ -420,8 +447,9 @@ module "create_in_ssm" {
     },
     secure_parameters = merge(local.qatalyst_ssm_secure_values,
       {
-        join("-", ["qatalyst", var.STAGE, "open-ai-key"]) = lookup(var.open_ai_api, data.aws_region.in.name)
-        "qatalyst-dashboard-opensearch-endpoint"          = join("", ["https://", try(module.create_in_opensearch[0].opensearch_host, "NA")])
+        join("-", ["qatalyst", var.STAGE, "open-ai-key"])       = lookup(var.open_ai_api, data.aws_region.in.name)
+        "qatalyst-dashboard-opensearch-endpoint"                = join("", ["https://", try(module.create_in_opensearch[0].opensearch_host, "NA")])
+        join("-", ["qatalyst", var.STAGE, "feature-flag-auth"]) = random_uuid.feature_flag_auth_in.result
     })
   }
 
@@ -678,8 +706,9 @@ module "create_sea_ssm" {
     },
     secure_parameters = merge(local.qatalyst_ssm_secure_values,
       {
-        join("-", ["qatalyst", var.STAGE, "open-ai-key"]) = lookup(var.open_ai_api, data.aws_region.sea.name)
-        "qatalyst-dashboard-opensearch-endpoint"          = join("", ["https://", try(module.create_sea_opensearch[0].opensearch_host, "NA")])
+        join("-", ["qatalyst", var.STAGE, "open-ai-key"])       = lookup(var.open_ai_api, data.aws_region.sea.name)
+        "qatalyst-dashboard-opensearch-endpoint"                = join("", ["https://", try(module.create_sea_opensearch[0].opensearch_host, "NA")])
+        join("-", ["qatalyst", var.STAGE, "feature-flag-auth"]) = random_uuid.feature_flag_auth_sea.result
     })
   }
   providers = {
@@ -1099,8 +1128,9 @@ module "create_us_ssm" {
     },
     secure_parameters = merge(local.qatalyst_ssm_secure_values,
       {
-        join("-", ["qatalyst", var.STAGE, "open-ai-key"]) = lookup(var.open_ai_api, data.aws_region.us.name)
-        "qatalyst-dashboard-opensearch-endpoint"          = join("", ["https://", try(module.create_us_opensearch[0].opensearch_host, "")])
+        join("-", ["qatalyst", var.STAGE, "open-ai-key"])       = lookup(var.open_ai_api, data.aws_region.us.name)
+        "qatalyst-dashboard-opensearch-endpoint"                = join("", ["https://", try(module.create_us_opensearch[0].opensearch_host, "")])
+        join("-", ["qatalyst", var.STAGE, "feature-flag-auth"]) = random_uuid.feature_flag_auth_us.result
     })
   }
   providers = {
